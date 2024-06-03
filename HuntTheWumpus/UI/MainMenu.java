93e8671346f58f855e3100b062cc580e73130e3d
iamthemusic <98472879+iamthemusic@users.noreply.github.com>
2024-06-03 13:23:08 -0700

    Update MainMenu.java

--- HuntTheWumpus/UI/MainMenu.java
+++ HuntTheWumpus/UI/MainMenu.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
 
 public class MainMenu extends JFrame implements ActionListener {
 
@@ -36,7 +37,8 @@ public class MainMenu extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         
         if (e.getActionCommand().equals("Play")) {
-            new UITest();
+                new UITest();
+
             dispose();
         }
     }

4d0071d3aac1e957a8ac4b1e091cbf9999cb0742
iamthemusic <98472879+iamthemusic@users.noreply.github.com>
2024-06-03 13:22:10 -0700

    Update UITest.java

--- HuntTheWumpus/UI/UITest.java
+++ HuntTheWumpus/UI/UITest.java
@@ -1,9 +1,14 @@
 package UI;
 
 import javax.swing.*;
+
+import Cave.Cave;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class UITest extends JFrame implements ActionListener {
 
@@ -11,6 +16,7 @@ public class UITest extends JFrame implements ActionListener {
     private static final int HEX_WIDTH = (int) (Math.sqrt(3) * HEX_SIZE);
     private static final int HEX_HEIGHT = HEX_SIZE * 2;
     private String number;
+    private Cave cave;
 
     public UITest() {
         draw();
@@ -21,13 +27,11 @@ public class UITest extends JFrame implements ActionListener {
     //////////////////////
 
     public void draw() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLayout(null);
-        setSize(800, 800);
-        setBackground(Color.BLACK);
-        setForeground(Color.BLACK);
+        this.getContentPane().setBackground(Color.LIGHT_GRAY);
 
-        int buttonNumber = 1;
+        setSize(8000,8000);
 
         // Pattern as shown in the image
         int[][] pattern = {
@@ -36,7 +40,6 @@ public class UITest extends JFrame implements ActionListener {
             {13, 14, 15, 16, 17, 18},
             {19, 20, 21, 22, 23, 24},
             {25, 26, 27, 28, 29, 30}
-          
         };
 
         for (int y = 0; y < pattern.length; y++) {
@@ -52,27 +55,39 @@ public class UITest extends JFrame implements ActionListener {
 
     private void addHexagonButton(int x, int y, int number) {
         HexagonButton button = new HexagonButton(String.valueOf(number));
-        int posX = x * (HEX_WIDTH * 3 / 4);
-        int posY = y * HEX_HEIGHT + (x % 2) * (HEX_HEIGHT / 2);
+        int posX = x * (HEX_WIDTH * 3 / 4) + 500; 
+        int posY = y * (HEX_HEIGHT * 3 / 4) + (x % 2) * (HEX_HEIGHT / 2) + 200; 
+        if (x % 2 == 0) {
+            posY += 6; 
+        }
+        
         button.setBounds(posX, posY, HEX_WIDTH, HEX_HEIGHT);
-        button.setBackground(Color.DARK_GRAY);
-        button.setForeground(Color.BLACK);
+        button.setBackground(Color.WHITE);
         button.addActionListener(this);
         add(button);
-    }
 
-    public static void main(String[] args) {
-        new UITest();
+    }
+    
+    public void highlightButton(int number){
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        HexagonButton button = (HexagonButton) e.getSource();
-        this.number = button.getText();
-        System.out.println(number);
+        try {
+            HexagonButton button = (HexagonButton) e.getSource();
+            button.setForeground(new Color(255, 0, 0)); // set color of clicked button red
+            this.number = button.getText();
+            int num = Integer.parseInt(number);
+            //ArrayList<Integer> nums = cave.getNeighbors(num);
+            System.out.println(number);
+        } catch (Exception ex) {
+            // Handle other potential exceptions
+            JOptionPane.showMessageDialog(this, "An error occurred: " + ex.getMessage());
+        }
     }
-    
-    public String getNumber(){
+
+    public String getNumber() {
         return this.number;
     }
 }

07965b5f7fac5b3d0b579bcdd732aa6fa60bb47d
xAtlanticMitroX <amritgill0428@outlook.com>
2024-06-06 21:52:23 -0700

    modified:   HuntTheWumpus/Sound/SoundPlayer.java

--- HuntTheWumpus/Sound/SoundPlayer.java
+++ HuntTheWumpus/Sound/SoundPlayer.java
@@ -1,6 +1,8 @@
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javazoom.jl.player.Player;
+import java.io.FileInputStream;
 
 public class SoundPlayer {
     public static void SoundPlayer(String[] args) {
@@ -65,8 +67,26 @@ public class SoundPlayer {
 
     public void lose(){
         try {
-            // Provide the path to your .wav file
+            // Provide the path to your MP3 file
             String filePath = "HuntTheWumpus\Sound\SoundEffects\lose.mp3";
+
+            // Create a FileInputStream for the MP3 file
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+
+            // Create a new Player instance
+            Player player = new Player(fileInputStream);
+
+            // Start playing the MP3
+            player.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void win(){
+        try {
+            // Provide the path to your .wav file
+            String filePath = "HuntTheWumpus\Sound\SoundEffects\win.wav";
 
             // Create an AudioInputStream from the file
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(filePath));
@@ -86,6 +106,24 @@ public class SoundPlayer {
             // Close the resources
             clip.close();
             audioInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void wrong(){
+        try {
+            // Provide the path to your MP3 file
+            String filePath = "HuntTheWumpus\Sound\SoundEffects\wrong.mp3";
+
+            // Create a FileInputStream for the MP3 file
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+
+            // Create a new Player instance
+            Player player = new Player(fileInputStream);
+
+            // Start playing the MP3
+            player.play();
         } catch (Exception e) {
             e.printStackTrace();
         }
